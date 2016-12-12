8c744706685e72d43ce1ff4513ca18540ee3aa01
Eric Love <io@ericlove.me>
2016-12-12 15:33:44 -0800

    New aliases for git commands

--- common/.common_profile.sh
+++ common/.common_profile.sh
@@ -2,6 +2,8 @@ alias uno="git status -uno"
 alias glg='git log'
 alias gst='git status'
 alias gdf='git diff'
+alias gbr='git branch'
+alias gcomm='git commit'
 
 export VISUAL=vim
 source ~/.local_profile.sh
