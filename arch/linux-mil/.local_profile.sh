72981e21a92776304d2a051cbb8720e321d1436f
Eric Love <ericlove@berkeley.edu>
2016-12-12 15:02:48 -0800

    Better handling of PATH

--- arch/linux-mil/.local_profile.sh
+++ arch/linux-mil/.local_profile.sh
@@ -1,5 +1,41 @@
 export SCRATCH_HOME=/scratch/ericlove
 
+# Only save original PATH value once per shell session
+if [ -z "$LINMIL_ORIG_PATH" ]; then
+  export LINMIL_ORIG_PATH=$PATH
+fi
+
+echo EXCUTED LOCAL PROFILE
+
+function set_path () {
+  # Always start setting LOCAL_PATH with this line
+  LOCAL_PATH=$SCRATCH_HOME/install/bin
+
+  if [ -n "$RISCV_PATH" ]; then
+    LOCAL_PATH=$RISCV_PATH/bin:$LOCAL_PATH
+  fi
+
+  if [ -z "$LINMIL_ORIG_PATH" ]; then
+    export PATH=$LOCAL_PATH
+  else
+    export PATH=$LOCAL_PATH:$PATH
+  fi
+}
+
+export CPATH=$SCRATCH_HOME/install/include
+
+export LD_LIBRARY_PATH=$SCRATCH_HOME/install/lib:$SCRATCH_HOME/install/lib64
+
 function cds () {
   cd $SCRATCH_HOME;
+}
+
+function use_riscv () {
+  export RISCV_PATH=$SCRATCH_HOME/riscv
+  set_path;
+}
+
+function no_riscv () {
+  unset RISCV_PATH
+  set_path;
 }
--- common/.bashrc
+++ common/.bashrc
@@ -1,1 +1,13 @@
-export PATH=$HOME/bin:$HOME/scripts:$PATH
+MY_PATH=$HOME/bin:$HOME/scripts
+
+if [ -z "$RC_ORIG_PATH" ]; then
+  echo "RC_ORIG_PATH=$PATH"
+  export RC_ORIG_PATH=$PATH
+fi
+
+if [ -z "$PATH" ]; then
+  export PATH=$MY_PATH
+else
+  export PATH=$MY_PATH:$RC_ORIG_PATH
+fi
+
