77eb2992d8110fb5626e78b2814283782a0b4538
Eric Love <io@ericlove.me>
2016-12-12 15:34:28 -0800

    Move .bash_profile into mac local

--- arch/mac/.local_profile.sh
+++ arch/mac/.local_profile.sh
@@ -1,1 +1,29 @@
-# empty for now
+
+if [ -z "$MAC_ORIG_PATH" ]; then
+  export MAC_ORIG_PATH=$PATH
+fi
+
+function set_mac_path () {
+  # Always start setting LOCAL_PATH with this line
+  LOCAL_PATH="~/bin/:~/local/bin:$MAC_ORIG_PATH"
+
+  # MacPorts Installer addition on 2015-11-21_at_16:39:05: adding an appropriate PATH variable for use with MacPorts.
+  LOCAL_PATH="/opt/local/bin:/opt/local/sbin:$LOCAL_PATH"
+  # Finished adapting your PATH environment variable for use with MacPorts.
+
+  LOCAL_PATH="/usr/local/texlive/2014/bin/x86_64-darwin:$LOCAL_PATH"
+
+  LOCAL_PATH="~/scripts:$LOCAL_PATH"
+
+  # added by Anaconda2 4.1.1 installer
+  LOCAL_PATH="~/anaconda/bin:$LOCAL_PATH"
+
+
+  if [ -z "$MAC_ORIG_PATH" ]; then
+    export PATH="$LOCAL_PATH"
+  else
+    export PATH="$LOCAL_PATH"
+  fi
+}
+
+set_mac_path
