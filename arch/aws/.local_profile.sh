4ba10888577b72c665e29fb4f8eca380d3a8a3ec
Eric Love <io@ericlove.me>
2017-10-25 17:56:41 -0700

    add force flag

--- arch/aws/.local_profile.sh
+++ arch/aws/.local_profile.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH=~/libinst
 export LIBINST_QUILT_PACK="ericlove/cpuperf"
 cd ~/dbbench; git pull
-echo "import quilt; quilt.install(\"ericlove/cpuperf\")" | python
+echo "import quilt; quilt.install(\"ericlove/cpuperf\", force=True)" | python
